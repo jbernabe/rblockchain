0974923e528c53658a2746780f2fcb24242606ab
jbernabe <jbernabemoreno@gmail.com>
2017-12-24 12:16:14 +0100

    Update blockchain.R

--- blockchain.R
+++ blockchain.R
@@ -1,3 +1,10 @@
+list.of.packages <- c("digest", "httr","jsonlite")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+require(digest)
+require(jsonlite)
+require(httr)
+
 Blockchain <- function ()
 {
   bc = list (
